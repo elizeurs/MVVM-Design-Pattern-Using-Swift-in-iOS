3bf030a0186f976fd458550b34d495478ab407b1
Elizeu RS <13ders@gmail.com>
2022-08-21 16:04:39 -0300

    Creating Models

---
+++ HotCoffee/HotCoffee/Models/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Elizeu RS on 21/08/22.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+  case cappuccino
+  case latte
+  case espresso
+  case cortado
+}
+
+enum CoffeeSize: String, Codable {
+  case small
+  case medium
+  case large
+}
+
+struct Order: Codable {
+  let name: String
+  let email: String
+  let type: CoffeeType
+  let size: CoffeeSize
+}
