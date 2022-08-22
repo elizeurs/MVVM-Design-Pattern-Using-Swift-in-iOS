bcb25035dc0e783ad5a359608b78af8eee6fa4bb
Elizeu RS <13ders@gmail.com>
2022-08-22 19:09:15 -0300

    Populating Types of Coffee

--- HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
@@ -8,6 +8,30 @@
 import Foundation
 import UIKit
 
-class AddOrderViewController: UIViewController {
+class AddOrderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
+  private var vm = AddCoffeeOrderViewModel()
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // alrealdy done via storyboard
+//    self.tableView.delegate = self
+//    self.tableView.dataSource = self
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return self.vm.types.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeTableViewCell", for: indexPath)
+    
+    cell.textLabel?.text = self.vm.types[indexPath.row]
+    return cell
+  }
 }
