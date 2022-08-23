7241bab79f8367555426a62e2e3b096d3e71ffed
Elizeu RS <13ders@gmail.com>
2022-08-23 15:58:25 -0300

    Populating AddCoffeeOrderViewModel

--- HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
@@ -12,6 +12,9 @@ class AddOrderViewController: UIViewController, UITableViewDelegate, UITableView
   
   private var vm = AddCoffeeOrderViewModel()
   @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var emailTextField: UITextField!
+  
   private var coffeeSizesSegmentedControl: UISegmentedControl!
   
   override func viewDidLoad() {
@@ -54,5 +57,23 @@ class AddOrderViewController: UIViewController, UITableViewDelegate, UITableView
     
     cell.textLabel?.text = self.vm.types[indexPath.row]
     return cell
+  }
+  
+  @IBAction func save() {
+    
+    let name = self.nameTextField.text
+    let email = self.emailTextField.text
+    
+    let selectedSize = self.coffeeSizesSegmentedControl.titleForSegment(at: self.coffeeSizesSegmentedControl.selectedSegmentIndex)
+    
+    guard let indexPath = self.tableView.indexPathForSelectedRow else { fatalError("Error in selecting coffee!")
+      
+    }
+    
+    self.vm.name = name
+    self.vm.email = email
+    
+    self.vm.selectedSize = selectedSize
+    self.vm.selectedType = self.vm.types[indexPath.row]
   }
 }
