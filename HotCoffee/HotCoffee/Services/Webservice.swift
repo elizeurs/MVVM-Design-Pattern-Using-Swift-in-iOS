fc7edd4415948a0ef060efd427e0ee01f0d12c2c
Elizeu RS <13ders@gmail.com>
2022-08-24 13:23:36 -0300

    Updating Webservice to Add Post

--- HotCoffee/HotCoffee/Services/Webservice.swift
+++ HotCoffee/HotCoffee/Services/Webservice.swift
@@ -34,7 +34,12 @@ class Webservice {
   
   func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
     
-    URLSession.shared.dataTask(with: resource.url) { data, response, error in
+    var request = URLRequest(url: resource.url)
+    request.httpMethod = resource.httpMethod.rawValue
+    request.httpBody = resource.body
+    request.addValue("application/json", forHTTPHeaderField: "Content-type")
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
       
       guard let data = data, error == nil else {
         completion(.failure(.domainError))
