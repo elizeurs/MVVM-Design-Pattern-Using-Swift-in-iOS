f01aee64f22402e3ba7e21ed271f6af52cc960e4
Elizeu RS <13ders@gmail.com>
2022-08-25 17:51:25 -0300

    Displaying Hard-Coded Weather into TableView

---
+++ GoodWeather/GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Elizeu RS on 25/08/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+  
+  @IBOutlet weak var cityNameLabel: UILabel!
+  @IBOutlet weak var temperatureLabel: UILabel!
+}
