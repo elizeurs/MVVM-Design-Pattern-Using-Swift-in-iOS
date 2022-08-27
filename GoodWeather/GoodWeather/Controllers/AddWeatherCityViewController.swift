d835fa67db25a9a5f300a867d03cf68f12880364
Elizeu RS <13ders@gmail.com>
2022-08-27 16:50:57 -0300

    Testing the Webservice Class

--- GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -14,6 +14,18 @@ class AddWeatherCityViewController: UIViewController {
   
   @IBAction func saveCityButtonPressed() {
     
+    if let city = cityNameTextField.text {
+      let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=7851ccae79e76935f159a49f6be64336&units=imperial")!
+      
+      let weatherResource = Resource<Any>(url: weatherURL) { data in
+        
+        return data
+      }
+      
+      Webservice().load(resource: weatherResource) { result in
+        
+      }
+    }
   }
   
   @IBAction func close() {
