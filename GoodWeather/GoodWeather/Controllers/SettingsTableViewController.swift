a66583eafc2769634f9215dea7e91fad4c182bf9
Elizeu RS <13ders@gmail.com>
2022-08-29 18:03:39 -0300

    Setting up SettingsTableViewController

---
+++ GoodWeather/GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Elizeu RS on 29/08/22.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+  
+}
