46b407a4819658fda8761b16c3d63cd70005b45e
Elizeu RS <13ders@gmail.com>
2022-09-02 15:52:25 -0300

    Implementing Webservice to Fetch News and Creating Models

---
+++ StocksAppSwiftUI/StocksAppSwiftUI/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  StocksAppSwiftUI
+//
+//  Created by Elizeu RS on 02/09/22.
+//
+
+import Foundation
+
+struct Article: Decodable {
+  let title: String
+  let imageURL: String
+  let publication: String
+}
--- StocksAppSwiftUI/StocksAppSwiftUI/Services/Webservice.swift
+++ StocksAppSwiftUI/StocksAppSwiftUI/Services/Webservice.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Webservice {
   
-  func getStocks(completion: @escaping (([Stock]?) -> Void)) {
+  func getTopNews(completion: @escaping (([Article]?) -> Void)) {
     
     guard let url = URL(string: "https://island-bramble.glitch.me/stocks") else {
       fatalError("Url is not correct")
@@ -17,13 +17,45 @@ class Webservice {
     
     URLSession.shared.dataTask(with: url) { data, response, error in
       
+      guard let data = data else {
+        DispatchQueue.main.async {
+          completion(nil)
+        }
+        return
+      }
+      
+      let articles = try? JSONDecoder().decode([Article].self, from: data)
+      DispatchQueue.main.async {
+        completion(articles)
+      }
+      
+    }.resume()
+    
+  }
+  
+  func getStocks(completion: @escaping (([Stock]?) -> Void)) {
+    
+    guard let url = URL(string: "https://island-bramble.glitch.me/top-news") else {
+      fatalError("Url is not correct")
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      
       guard let data = data, error == nil else {
-        completion(nil)
+        DispatchQueue.main.async {
+          completion(nil)
+        }
         return
       }
       
       let stocks = try? JSONDecoder().decode([Stock].self, from: data)
-      stocks == nil ? completion(nil) : completion(stocks)
+      DispatchQueue.main.async {
+        completion(stocks)
+      }
+      
+//      stocks == nil ? completion(nil) : completion(stocks)
+      
     }.resume()
+    
   }
 }
