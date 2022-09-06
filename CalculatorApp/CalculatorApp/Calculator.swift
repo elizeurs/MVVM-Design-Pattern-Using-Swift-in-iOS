9c669a609ff359851faa516d726fe0d059ec73cc
Elizeu RS <13ders@gmail.com>
2022-09-06 16:29:35 -0300

    Writing Unit Test

---
+++ CalculatorApp/CalculatorApp/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  CalculatorApp
+//
+//  Created by Elizeu RS on 06/09/22.
+//
+
+import Foundation
+
+class Calculator {
+  
+  func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+  }
+  
+  func subtract(_ a: Int, _ b: Int) -> Int {
+    return a - b
+  }
+  
+}
--- CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -6,7 +6,27 @@
 //
 
 import XCTest
+@testable import CalculatorApp
 
 class CalculatorAppTests: XCTestCase {
+  
+  func test_SubtractTwoNumbers() {
+    
+    let calculator = Calculator()
+    let result = calculator.subtract(5,2)
+    
+    XCTAssertEqual(result, 3)
+  }
+  
+  func test_AddTwoNumbers() {
+    
+    let calculator = Calculator()
+    let result = calculator.add(2,3)
+    
+    XCTAssertEqual(result, 5)
+    
+    // failed test
+//    XCTAssertEqual(result, 9)
 
+  }
 }
